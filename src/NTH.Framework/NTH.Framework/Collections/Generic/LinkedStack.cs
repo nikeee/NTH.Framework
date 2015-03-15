8e2916623ad09a3674dece632923520e8d5c740b
Niklas Mollenhauer <nikeee@outlook.com>
2015-03-15 13:18:07 +0100

    Add linked stack proposal

---
+++ src/NTH.Framework/NTH.Framework/Collections/Generic/LinkedStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NTH.Framework.Collections.Generic
+{
+    public struct LinkedStack<T>
+    {
+        private readonly LinkedStack<T>? _previous;
+        private readonly T _value;
+        private readonly int _itemCount;
+
+        internal LinkedStack(int itemCount, T firstValue, LinkedStack<T>? previous)
+        {
+            _itemCount = itemCount;
+            _value = firstValue;
+            _previous = previous;
+        }
+
+        public T Pop()
+        {
+            if (_itemCount == 0)
+                throw new InvalidOperationException("Stack is empty.");
+
+            var res = _value;
+            if (_previous.HasValue)
+                this = _previous.Value;
+            else if (_itemCount == 1 || _itemCount == 0)
+                this = new LinkedStack<T>();
+            return res;
+        }
+
+        public void Push(T value)
+        {
+            var newTop = new LinkedStack<T>(_itemCount + 1, value, this);
+            this = newTop;
+        }
+    }
+}
